67419422062f3af02a091509558fa59e64603d28
Frank Delporte <fdelporte@azul.com>
2023-01-16 18:54:52 +0100

    Added module-info

---
+++ core/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.lottie4j.core {
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.databind;
+
+    exports com.lottie4j.core.definition;
+    exports com.lottie4j.core.handler;
+    exports com.lottie4j.core.helper;
+    exports com.lottie4j.core.model;
+    exports com.lottie4j.core.model.keyframe;
+    exports com.lottie4j.core.model.shape;
+
+    opens com.lottie4j.core.model to com.fasterxml.jackson.databind;
+    opens com.lottie4j.core.model.keyframe to com.fasterxml.jackson.databind;
+    opens com.lottie4j.core.model.shape to com.fasterxml.jackson.databind;
+}
---
+++ fxplayer/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.lottie4j.fxplayer {
+    requires com.fasterxml.jackson.databind;
+    requires com.lottie4j.core;
+    requires javafx.graphics;
+
+    exports com.lottie4j.fxplayer to javafx.graphics;
+}
